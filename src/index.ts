b0c0f78435f8de7e58927fc45bab572960b973f2
THH <335303710@qq.com>
2021-11-01 23:56:21 +0800

    初始化简易模版

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+console.log('hello world')
+
+export const a = 100;
+
+const temp: boolean[] = []
+
+export const sayName = (name: string) => {
+  if(name === '1') {
+    return 100
+  }
+  return false
+}
